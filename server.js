81b329359d735edd7e9e03ef467a0ab5c75c7426
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-06-11 21:52:54 -0700

    initial file updates

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
-//importing helpers
+// helper function
 const helpers = require('./utils/helpers');
 
-//importing handlebars
+// handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({helpers});
 
-//connecting session to sequelize
+// session connection to sequelize database
 const session = require('express-session');
 
 const app = express();
@@ -18,7 +18,6 @@ const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-//creating new session
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -34,13 +33,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// set Handlebars as the default template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
 // turn on routes
-app.use(routes);
-
+// app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
