ce020c29bbe674c34aac87bcb325ac24dbc7944d
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-06-12 17:43:49 -0700

    fixing logout route

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,18 +3,15 @@ const { User } = require('../models');
 const userData = [
   {
     username: 'izabelapetrovicova',
-    email: "i.petrovicova@gmail.com",
     password: 'test1'
     
   },
   {
     username: 'izabelap',
-    email: "izabelap@gmail.com",
     password: 'test2'
   },
   {
     username: 'ip',
-    email: "ip@gmail.com",
     password: 'test3'
   }
 ];
