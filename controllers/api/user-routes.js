30d3a6f553b272ac09863db194863f3309c6bdbf
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-06-13 15:15:40 -0700

    adding error handling

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -76,7 +76,11 @@ router.post('/', (req, res) => {
 
         res.json(dbUserData);
         });
-    });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 
@@ -108,7 +112,11 @@ router.post('/login', (req, res) => {
       
             res.json({ user: dbUserData, message: 'You are now logged in!' });
         });
-    });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 
